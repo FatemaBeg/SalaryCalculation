b4549ac125432771c31ad3e9c58d9e6fed7a1df8
FatemaBeg <fatema007beg@gmail.com>
2019-10-14 23:50:02 +0600

    salary Calculation

---
+++ SalaryCalculatorAppPractice3/SalaryCalculatorUi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculatorAppPractice3
+{
+    public partial class SalaryCalculatorUi : Form
+    {
+        
+        public SalaryCalculatorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowMeSalaryButton_Click(object sender, EventArgs e)
+        {
+            TotalSalaryCalculation salary = new TotalSalaryCalculation();
+
+            salary.Name = employeeNameTextBox.Text;
+            salary.BasicSalary = Convert.ToDouble(basicAmountTextBox.Text);
+            salary.HomeRent = Convert.ToDouble(homeRentTextBox.Text);
+            salary.MedicalAllowance = Convert.ToDouble(medicalAllowanceTextBox.Text);
+            MessageBox.Show(salary.Report());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ SalaryCalculatorAppPractice3/TotalSalaryCalculation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorAppPractice3
+{
+    public class TotalSalaryCalculation
+    {
+        public string Name { get; set; }
+        public double BasicSalary { get; set; }
+        public double HomeRent { get; set; }
+        public double MedicalAllowance { get; set; }
+        public double ShowTotalSalary { get; set; }
+        public double HomeBasic { get; set; }
+        public double MedicalBasic { get; set; }
+        public double GetTotalSalary()
+        {
+            ShowTotalSalary = BasicSalary + PersentanceHomeSalary();
+            double ShowTotalSalary1 = ShowTotalSalary + PersentanceMedicalSalary();
+
+            return ShowTotalSalary1;
+        }
+        public double PersentanceHomeSalary()
+        {
+            HomeBasic = (BasicSalary * HomeRent)/100;
+            return HomeBasic;
+        }
+        public double PersentanceMedicalSalary()
+        {
+            MedicalBasic = (BasicSalary * MedicalAllowance) / 100;
+            return MedicalBasic;
+        }
+        public string Report()
+        {
+           string  ShowResul = "" + Name + " Your salary is:  " + GetTotalSalary().ToString();
+           return ShowResul;
+        }
+    }
+}
